125bd8c6c01510fefc531d5ea109a82654d505ab
Christine Mendoza <christine.mendoza@unc.edu>
2023-02-11 15:10:34 -0500

    template app

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS #comment this on deployment
+from api.TestApiHandler import TestApiHandler
 
-app = Flask(__name__)
+# Use the second line to serve the front end with the back end
+app = Flask(__name__, static_url_path='', static_folder='')
+# app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+CORS(app) #comment this on deployment
+api = Api(app)
+
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
+
+api.add_resource(TestApiHandler, '/flask/test')
