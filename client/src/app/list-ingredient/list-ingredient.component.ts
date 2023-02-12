6f4c1f3faec4d370904a0ccfd3ee8e7226d61596
Christine Mendoza <christine.mendoza@unc.edu>
2023-02-12 09:57:57 -0500

    complete impl of services

--- client/src/app/list-ingredient/list-ingredient.component.ts
+++ client/src/app/list-ingredient/list-ingredient.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IngredientService } from '../service/ingredient/ingredient.service';
 
 @Component({
   selector: 'app-list-ingredient',
   templateUrl: './list-ingredient.component.html',
   styleUrls: ['./list-ingredient.component.css']
 })
-export class ListIngredientComponent {
+export class ListIngredientComponent implements OnInit {
+  componentName: String;
+  Ingredients: any = [];
+  handleUpdateResponse: any;
+  handleError: any;
 
+  constructor(private ingredientService: IngredientService) {
+    this.componentName = "ingredients";
+ }
+  // Angular Init method, retrieve all notifications from database
+  ngOnInit(): void {
+    this.ingredientService.getIngredients().subscribe(res => {
+      console.log(res)
+      this.Ingredients = res;
+    });
+  }
 }
--- client/src/app/list-recipe-favorite/list-recipe-favorite.component.ts
+++ client/src/app/list-recipe-favorite/list-recipe-favorite.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../service/recipe/recipe.service';
 
 @Component({
   selector: 'app-list-recipe-favorite',
   templateUrl: './list-recipe-favorite.component.html',
   styleUrls: ['./list-recipe-favorite.component.css']
 })
-export class ListRecipeFavoriteComponent {
+export class ListRecipeFavoriteComponent implements OnInit {
+  componentName: String;
+  FavoriteRecipes: any = [];
+  handleUpdateResponse: any;
+  handleError: any;
 
+  constructor(private recipeService: RecipeService) {
+    this.componentName = "recipes";
+ }
+  // Angular Init method, retrieve all notifications from database
+  ngOnInit(): void {
+    this.recipeService.getFavoriteRecipes().subscribe(res => {
+      console.log(res)
+      this.FavoriteRecipes = res;
+    });
+  }
 }
